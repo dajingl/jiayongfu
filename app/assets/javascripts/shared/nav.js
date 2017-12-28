8cfbfe51bfb57434fcffd66e3768c37d74c74a6c
dj <dajing61@foxmail.com>
2017-12-28 21:35:13 +0800

    修改导航

--- app/assets/javascripts/shared/nav.js
+++ app/assets/javascripts/shared/nav.js
@@ -4,8 +4,10 @@ $(document).on('turbolinks:load', function () {
        console.log(1)
        if ($(this).attr("class") === "open") {
            $(this).attr("class", "close-x");
+           $(".right").removeClass("visible-md visible-lg");
        } else {
            $(this).attr("class", "open");
+           $(".right").addClass("visible-md visible-lg");
        }
 
    })
