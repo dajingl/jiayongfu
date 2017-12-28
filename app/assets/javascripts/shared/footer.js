f14d32bbfc0beca1029cb4677a0a8fa6cc439b00
chenzhiyuan <chenzy@sevencent.com.cn>
2017-12-28 14:21:18 +0800

    修复控制台报错（暂时屏蔽)

--- app/assets/javascripts/shared/footer.js
+++ app/assets/javascripts/shared/footer.js
@@ -1,16 +1,16 @@
-$(document).on('turbolinks:load', function () {
-    var footerFun = function () {
-        var windowH = $(window).height();
-        var bodyH = $('body').height();
-        var $diff = windowH - bodyH;
-        var $footer = $('.footer');
-     console.log(windowH ,'-----', bodyH , $diff)
-        if ($diff < 198) {
-            $footer.css('position', 'relative');
-        } else if ($diff > 198) {
-            $footer.css('position', 'fixed');
-        }
-    };
-    footerFun();
-
-});
+// $(document).on('turbolinks:load', function () {
+//     var footerFun = function () {
+//         var windowH = $(window).height();
+//         var bodyH = $('body').height();
+//         var $diff = windowH - bodyH;
+//         var $footer = $('.footer');
+//      console.log(windowH ,'-----', bodyH , $diff)
+//         if ($diff < 198) {
+//             $footer.css('position', 'relative');
+//         } else if ($diff > 198) {
+//             $footer.css('position', 'fixed');
+//         }
+//     };
+//     footerFun();
+//
+// });
